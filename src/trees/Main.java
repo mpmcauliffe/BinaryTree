0bfd5f7615f81f80ecf4d440a6ff0d6cd6dba166
mpmcauliffe <mcauliffe99@gmail.com>
2019-05-11 21:36:13 -0600

    update

--- src/trees/Main.java
+++ src/trees/Main.java
@@ -4,8 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // Create instance
         Tree intTree = new Tree();
 
+        // Fill tree with insert method()
         intTree.insert(25);
         intTree.insert(20);
         intTree.insert(15);
@@ -17,28 +19,33 @@ public class Main {
         intTree.insert(32);
         intTree.insert(17);
 
+        // Traverse tree with IN order
         System.out.print("\n\n");
         System.out.println("Traversing tree IN order");
         System.out.print("DATA = ");
         intTree.traverseInOrder();
         System.out.print("\n\n");
 
+        // Traverse tree PRE order
         System.out.println("Traversing tree PRE order");
         System.out.print("DATA = ");
         intTree.traversePreorder();
         System.out.print("\n\n");
 
+        // Search tree with get method()
         System.out.println("Searching tree");
         System.out.println(intTree.get(27));
         System.out.println(intTree.get(17));
         System.out.println(intTree.get(8888));
         System.out.print("\n\n");
 
+        // Find min/max values
         System.out.println("Min and max values");
         System.out.println(intTree.min());
         System.out.println(intTree.max());
         System.out.print("\n\n");
 
+        // Remove nodes from tree use delete method()
         intTree.delete(15);
         System.out.println("Traversing tree post delete");
         System.out.print("DATA = ");
@@ -63,6 +70,19 @@ public class Main {
         intTree.traverseInOrder();
         System.out.print("\n\n");
 
+        // Insert new nodes into tree with insert()
+        intTree.insert(44);
+        intTree.insert(7);
+        intTree.insert(21);
+
+        // Traverse IN order
+        System.out.print("\n\n");
+        System.out.println("Traversing tree IN order");
+        System.out.print("DATA = ");
+        intTree.traverseInOrder();
+        System.out.print("\n\n");
+
+        // END
         System.out.print("\n\n");
     }
 }
